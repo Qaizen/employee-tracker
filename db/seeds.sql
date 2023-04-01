516ec838065f0fc2aa8e0265dba443fab8661468
Steven Nguyen <qviet715@gmail.com>
2023-04-01 18:28:19 -0400

    connection.js

--- db/seeds.sql
+++ db/seeds.sql
@@ -20,11 +20,11 @@ VALUES
 
 INSERT INTO employee(first_name, last_name, role_id, manager_id) 
 VALUES  
-    ('Bethany', 'Christian', 1, Null),
-    ('Chandler', 'Acevedo', 2, Null),
+    ('Bethany', 'Harris', 1, Null),
+    ('Alex', 'Pereria', 2, Null),
     ('Bob', 'Smith', 3, Null),
     ('Sally', 'Fields', 4, Null),
-    ('Mariella', 'Lewis', 5, 1),
-    ('Lola', 'Morris', 6, 2),
-    ('Shelly', 'Dixon', 7, 3),
-    ('Yasir', 'Sargent', 8, 4);
+    ('Bill', 'Wallace', 5, 1),
+    ('Rick', 'Morty', 6, 2),
+    ('Bruce', 'Lee', 7, 3),
+    ('Sen', 'Chihiro', 8, 4);
